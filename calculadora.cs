080bdf20eea159994779616143dd568dba535184
Cable16 <teodiobolvasor@gmail.com>
2025-07-28 20:33:37 -0400

    Funcionalidad suma implementada

--- calculadora.cs
+++ calculadora.cs
@@ -1,1 +1,1 @@
-"// Calculadora b sica using System;class Program {    static void Main() {        Console.WriteLine("Bienvenido a la Calculadora B sica");    }}" 
+"// Calculadora b sica using System;class Program {    static void Main() {        Console.WriteLine("Bienvenido a la Calculadora B sica");    }} Console.Write("Ingrese primer n£mero: ");int a = int.Parse(Console.ReadLine());Console.Write("Ingrese segundo n£mero: ");int b = int.Parse(Console.ReadLine());Console.WriteLine($"Resultado: {a + b}");" 
